0d4d3e4d42faf81d336629204e6f38e1e8d30164
Ada ATCHINARD <satchinard@cagecfi.com>
2015-04-24 14:36:51 +0000

    First commit

---
+++ concours-securite-web/target/classes/concours/App.java
@@ -0,0 +1,13 @@
+package concours;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
